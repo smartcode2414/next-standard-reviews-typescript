541ba2ddf0ef90f8f196bc467e5fd15941b6ea92
smartstandard <smartcode2414@gmail.com>
2024-09-22 21:22:13 -0400

    feat:add marked module

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,14 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import { readFile } from "node:fs/promises";
+import { marked } from "marked";
 
 export default async function StardewValleyPage() {
   const text = readFile('./content/reviews/stardew-valley.md', 'utf8')
+  const html = marked(await text, { headerIds: false, mangle: false });
 
   return (
     <>
       <h1>StardewValley</h1>
-      <img src="/images/stardew.png" alt="" width='640' height='360' className="rounded mb-2"/>
+      <img src="/images/stardew.png" alt="" width='640' height='360' className="rounded mb-2" />
+      <article dangerouslySetInnerHTML={{__html: html}}></article>
       <p>
-        {text}
+        {html}
       </p>
     </>
   );
